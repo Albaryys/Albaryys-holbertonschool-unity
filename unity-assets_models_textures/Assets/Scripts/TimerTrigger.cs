dfc56f6ab4c5025e478287f5baa7be58b153bcdf
Albaryys <a.tily.pro@gmail.com>
2024-02-22 13:49:42 +0100

    add .cs files

---
+++ unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().gameObject;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        player.GetComponent<Timer>().enabled = true;
+    }
+}
